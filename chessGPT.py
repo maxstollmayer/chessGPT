b1cdd12f967a399d8d6b13d3e4367575d6a9032c
Max <max.stolly@gmail.com>
2023-03-11 22:46:56 +0100

    Use openai api for bot

--- chessGPT.py
+++ chessGPT.py
@@ -2,8 +2,11 @@ import argparse
 from random import choice
 import re
 import sys
-import chess
 
+import chess
+import openai
+
+MODEL = "gpt-3.5-turbo"
 SAN_PATTERN = r"([Oo0](-[Oo0]){1,2}|[KQRBN]?[a-h]?[1-8]?x?[a-h][1-8](\=[QRBN])?[+#]?(\s(1-0|0-1|1\/2-1\/2))?)"
 SYMBOLS = {
     "K": "♔",
@@ -58,48 +61,37 @@ def get_user_move(board: chess.Board) -> chess.Move:
             san = input("This move is ambiguous. Also state piece to move: ")
 
 
-def get_assistant_prompt(color: str, board: str) -> str:
-    return f"\nWhat would be your next move for {color} on the following chess board, where each piece is represented by a single character. The uppercase letters represent the white pieces (K for king, Q for queen, R for rook, B for bishop, N for knight and P for pawn), while the corresponding lowercase letters represent the black pieces. The dots represent empty squares on the board. The ranks are numbered from 1 to 8, and the files are labeled from a to h. Start your reply with the {color}'s move in standard algebraic chess notation and an explanation afterwards but do not reprint the board.\n\n```\n{board}\n```"
+def get_ai_prompt(color: str, board_str: str) -> list[dict[str, str]]:
+    return [
+        {
+            "role": "system",
+            "content": f"You are a chess bot playing as {color}. You reply with the optimal next move in standard algebraic chess notation on the first line and nothing else and on the second line an explanation of the move but without reprinting the board. The user will give you a chess board configuration, where each piece is represented by a single character. The uppercase letters represent the white pieces (K for king, Q for queen, R for rook, B for bishop, N for knight and P for pawn), while the corresponding lowercase letters represent the black pieces. The dots represent empty squares on the board. The ranks are numbered from 1 to 8, and the files are labeled from a to h.",
+        },
+        {"role": "assistant", "content": "Ok."},
+        {
+            "role": "user",
+            "content": board_str,
+        },
+    ]
 
 
-def get_completion_prompt(color: str, board_str: str) -> str:
-    raise NotImplementedError()
+def send_ai_prompt(prompt: list[dict[str, str]]) -> str:
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=prompt,
+    )
+    return response["choices"][0]["message"]["content"]
 
 
-def send_completion_prompt(prompt: str) -> str:
-    raise NotImplementedError()
-
-
-def get_assistant_move(board: chess.Board) -> chess.Move:
+def get_ai_move(board: chess.Board, max_tries: int) -> chess.Move:
     color = "white" if board.turn == chess.WHITE else "black"
-    prompt = get_assistant_prompt(color, str(board))
-    print(prompt)
-    san = input("\nCopy the above prompt into chatGPT and paste the move here: ")
-    while True:
-        if san == "quit":
-            sys.exit()
-        try:
-            move = board.parse_san(san.strip())
-            return move
-        except chess.IllegalMoveError:
-            san = input(
-                "Move is illegal. Regenerate chatGPT's response for a new move: "
-            )
-        except ValueError:
-            san = input(
-                "Move was invalid. Make sure you paste correct notation or regenerate chatGPT's response: "
-            )
-
-
-def get_completion_move(board: chess.Board, max_tries: int) -> chess.Move:
-    color = "white" if board.turn == chess.WHITE else "black"
-    prompt = get_completion_prompt(color, str(board))
+    prompt = get_ai_prompt(color, str(board))
     for _ in range(max_tries):
-        reply = send_completion_prompt(prompt)
+        reply = send_ai_prompt(prompt)
         try:
-            san = next(re.finditer(SAN_PATTERN, reply)).string
+            san = next(re.finditer(SAN_PATTERN, reply)).group()
             move = board.parse_san(san)
-            print("AI explanation:", reply)
+            print("\n".join([line for line in reply.splitlines()[1:] if line != ""]))
             return move
         except StopIteration:
             continue
@@ -109,7 +101,21 @@ def get_completion_move(board: chess.Board, max_tries: int) -> chess.Move:
     return chess.Move.null()
 
 
+def authenticate() -> None:
+    if openai.api_key is None:
+        print("OpenAI API key not found in environment variable 'OPENAI_API_KEY'.")
+        openai.api_key = input("Enter your OpenAI key manually: ")
+    try:
+        openai.Model.retrieve(MODEL)
+    except openai.error.AuthenticationError as err:
+        print(err)
+        sys.exit()
+
+
 def main() -> None:
+    # read api key
+    authenticate()
+
     print("\n")
     # argument parsing
     parser = argparse.ArgumentParser(
@@ -122,15 +128,9 @@ def main() -> None:
         help="start as white otherwise determines sides randomly",
     )
     parser.add_argument(
-        "-a",
-        "--assistant",
-        action="store_false",
-        help="use chatGPT interface instead of the GPT3 API",
-    )
-    parser.add_argument(
         "-t",
         "--tries",
-        default=10,
+        default=1,
         help="maximum number of tries for AI to generate a valid move, makes no move otherwise",
     )
     args = parser.parse_args()
@@ -146,15 +146,12 @@ def main() -> None:
 
     # game loop
     while not board.is_game_over():
-        if args.assistant:
-            board.push(get_assistant_move(board))
-        else:
-            board.push(get_completion_move(board, args.tries))
+        board.push(get_ai_move(board, args.tries))
         print(render(board))
         board.push(get_user_move(board))
 
     # print outcome
-    winner = board.outcome().winner  # type: ignore
+    winner = board.outcome().winner
     if winner is None:
         print("It's a draw.")
     elif winner == user_side:
